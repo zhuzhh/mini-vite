59c964ba53fbb67e43cfcc2da558fa186a7580bd
zhuzhh <zhuzhh666@163.com>
2022-09-09 18:10:27 +0800

    server done

--- src/node/cli.ts
+++ src/node/cli.ts
@@ -1,5 +1,5 @@
 import cac from "cac";
-
+import { startDevServer } from "./server";
 const cli = cac();
 
 // [] 中的内容为可选参数，也就是说仅输入 `vite` 命令下会执行下面的逻辑
@@ -8,9 +8,9 @@ cli
   .alias("serve")
   .alias("dev")
   .action(async () => {
-    console.log('测试 cli~');
+    await startDevServer()
   });
 
 cli.help();
 
-cli.parse();
+cli.parse()
